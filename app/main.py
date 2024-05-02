b6532e786998e4fe425d50002b915cb0fa8a9d41
Kaosi Anikwe <anikwehenryasa@gmail.com>
2024-05-02 13:23:19 +0000

    change output format

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 # python imports
 import os
-import base64
 import traceback
 
 # installed imports
@@ -22,10 +21,14 @@ def get_images(damage):
         # Read images from folder
         for filename in os.listdir(image_folder):
             if filename.endswith(".jpeg") or filename.endswith(".png"):
-                with open(os.path.join(image_folder, filename), "rb") as f:
-                    # Convert image to base64
-                    base64_img = base64.b64encode(f.read()).decode("utf-8")
-                    images.append({"filename": filename, "data": base64_img})
+                coordinate = os.path.splitext(os.path.basename(filename))[0]
+                images.append({
+                    "filename": filename,
+                    "coordinates": {
+                        "latitude": coordinate.split("_")[0],
+                        "longitude": coordinate.split("_")[1],
+                    }
+                })
         logger.info(f"Returning {len(images)} images")
         return jsonify(images=images, success=True, message="")
     except Exception as e:
